dafc34843e24bd5756b397d9f8b93298297d3e05
Rodrigo Ramos <rodrigomramos18@hotmail.com>
2022-07-13 16:32:49 -0300

    rodrigo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import HeaderPrincipal from './Componentes/headerPrincipal';
-import HeaderSecundario from './Componentes/headerSecundario';
-import CadastroAdmin from './Pages/cadastroAdmin';
-import CadastroCliente from './Pages/cadastroCliente';
-import Login from './Pages/Login';
-import RecuperarSenha from './Pages/RecuperarSenha';
+
 
 
 ReactDOM.render(
